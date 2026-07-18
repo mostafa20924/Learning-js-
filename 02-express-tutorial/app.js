58f6a2884fa3f74b322474fb5e81ed8ae9fe37b8
Mostafa <moustafa.20924@gmail.com>
2026-07-18 23:48:17 +0300

    Initial commit

---
+++ 02-express-tutorial/app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Home Page</h1>');
+    res.end();
+});
+server.listen(5001); 
