35cc0c0b74573aeb4f8227e8baba88e6b5e558a1
Mostafa Khalil work <moustafa.20924@gmail.com>
2026-08-22 17:17:53 +0300

    Update code

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,26 +1,31 @@
 const express = require('express')
 const app = express();
-const morgan = require('morgan');
-const logger = require('./logger');
-const authorize = require('./authorize');
+let {people} = require('./data');
 
 
-//app.use([logger,authorize]);
-app.use(morgan('tiny'));
-app.get('/', (req, res) => {
-    res.send('<h1>Home Page</h1>');
+app.use(express.static('./methods-public'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get('/api/people', (req, res) => {
+    res.status(200).json({success: true, data: people});
 });
 
-app.get('/about', (req, res) => {
-    res.send('<h1>About Page</h1>');
-});
-app.get('/api/products', (req, res) => {
-    res.send('<h1>Products Page</h1>');
-});
-app.get('/api/items', (req, res) => {
-    res.send('<h1>Items Page</h1>');
+app.post('/api/people', (req, res) => {
+    const {name} = req.body;
+    if(!name){
+        return res.status(400).json({success: false, msg: 'Please provide name value'});
+    }
+   res.status(201).json({success: true,person : name, msg: 'Person added successfully'});
 });
 
+app.post('/api/people', (req, res) => {
+    const {name} = req.body;
+    if(name){
+        return res.status(200).send(`Welcome ${name}`);
+    }
+    res.status(400).json({success: false, msg: 'Please provide name value'});
+});
 app.listen(5001, () => {
     console.log('Server listening on port 5001');
 });
